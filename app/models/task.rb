473d6c3cd0e520c66f8a65e0309007f5b656a713
Shannon Drew <shannon.nicole.drew@gmail.com>
2023-06-25 19:26:01 -0700

    archive feature available

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,7 @@ class Task < ApplicationRecord
         started:  1,
         finished: 2
     }
+
+    scope :deleted, -> {where("deleted_at IS NOT NULL")}
+    scope :active, -> {where("deleted_at IS NULL")}
 end
